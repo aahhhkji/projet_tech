682d30e6bd558c9ea2414e9e1aa265d0718de453
FATIMA ZAHRA MARZAQUI <122360476+aahhhkji@users.noreply.github.com>
2023-05-26 17:36:22 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -1,36 +1,37 @@
+<?php
+require_once('functions.php');
 
-<?php require_once('functions.php');
-  
+// Vérifier si l'utilisateur est connecté
+if (!isset($_SESSION['user'])) {
+    header('Location: login.php');
+    exit();
+}
 
+$bdd = connect();
 
-    if (!isset($_SESSION['user'])) {
-        header('Location: login.php');
-    }
+$sql = "SELECT * FROM oeuvre WHERE categories_id = :categories_id";
 
-    $bdd = connect();
+$sth = $bdd->prepare($sql);
 
-    $sql = "SELECT * FROM oeuvre WHERE user_id = :user_id";
+$sth->execute([
+    'categories_id' => $_SESSION['user']['id']
+]);
 
-    $sth = $bdd->prepare($sql);
-        
-    $sth->execute([
-        'user_id'     => $_SESSION['user']['id']
-    ]);
-
-    $persos = $sth->fetchAll();
-
-    // dd($persos);
+$persos = $sth->fetchAll();
 
 ?>
-<?php require_once('_header.php');
-require_once('barre.php'); ?>
 
+<?php require_once('_header.php'); ?>
+<?php require_once('barre.php'); ?>
 
-<h1>Bienvenue au musee sensoriel</h1>
+<h1>Bienvenue au musée sensoriel</h1>
 
+<div class="rectangle">
+    <h4>A propos de nous</h4>
+</div>
 
+<!-- Ajoutez ici le reste de votre contenu HTML -->
 
-<div class="rectangle"> <h4>a propos de nous</h4></div>
-
-
-
+<!-- Fermez les balises HTML manquantes -->
+</body>
+</html>
